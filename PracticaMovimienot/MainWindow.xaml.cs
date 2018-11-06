04134a4173ccf89cec5af067080552c5c0280cce
Karly-98 <Karly-98@DESKTOP-D2ALSSH>
2018-11-06 08:29:56 -0700

    Movimiento y Carro

--- PracticaMovimienot/MainWindow.xaml.cs
+++ PracticaMovimienot/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+///Librerias para multiprocesamientos
+using System.Threading; //Se agrego esta libreria para Threading
+using System.Diagnostics; // Para el dispatcher y timers 
+
 
 namespace PracticaMovimienot
 {
@@ -20,10 +24,48 @@ namespace PracticaMovimienot
     /// </summary>
     public partial class MainWindow : Window
     {
+        Stopwatch stopwatch; //Toma el tiempo de ejecución del programa
+        TimeSpan tiempoAnterior; //Timespan guarda rangos de tiempo
+
+
         public MainWindow()
         {
             InitializeComponent();
             elCanvas.Focus();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+
+            //Se puede mandar como parametro a una función en C#
+            //1..Establecer instrucciones
+            ThreadStart threadStart = new ThreadStart(moverEnemigos);
+            //2..Inicializar el Thread - Dar valores e instrucciones
+            Thread threadMoverEnemigos = new Thread(threadStart);
+            //3..Ejecutar el Thread
+            threadMoverEnemigos.Start();
+        }
+
+        void moverEnemigos()
+        {   //Invoke lleva de parametro una función
+            while (true) { 
+            Dispatcher.Invoke(
+                () => //Se creo una función nueva dentro de otro la => es para indicar que es otra función
+                {
+                    var tiempoActuali = stopwatch.Elapsed;
+                    var deltaTime = tiempoActuali - tiempoAnterior;
+
+                        double leftCarroActual = Canvas.GetLeft(imgCarro);
+                                                                   // se mueve 120 pixeles por segundo
+                        Canvas.SetLeft(imgCarro, leftCarroActual - (120* deltaTime.TotalSeconds));
+                    if (Canvas.GetLeft(imgCarro) <= -100)
+                    {
+                        Canvas.SetLeft(imgCarro, 800);
+                    }
+                    tiempoAnterior = tiempoActuali;
+                }
+                );
+            }
         }
 
         private void elCanvas_KeyDown(object sender, KeyEventArgs e)
@@ -36,16 +78,21 @@ namespace PracticaMovimienot
             }
             if (e.Key == Key.Down)
             {
-                double bottomTigreActual = Canvas.GetBottom(imgTigre);
-                Canvas.SetBottom(imgTigre, bottomTigreActual + 15);
+                double bottomTigreActual = Canvas.GetTop(imgTigre);
+                Canvas.SetTop(imgTigre, bottomTigreActual + 15);
+            }
+
+            if (e.Key == Key.Left)
+            {
+                double LeftTigreActual = Canvas.GetLeft(imgTigre);
+                Canvas.SetLeft(imgTigre, LeftTigreActual - 15);
+            }
+
+            if (e.Key == Key.Right)
+            {
+                double RightTigreActual = Canvas.GetLeft(imgTigre);
+                Canvas.SetLeft(imgTigre, RightTigreActual + 15);
             }
         }
-
-        private void elCanvas_GotFocus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        
     }
 }
